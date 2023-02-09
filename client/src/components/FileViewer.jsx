59e4d6c744e2873276a348fe62c917f3b8c03109
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-02-09 13:27:04 +0530

    added server command & added no files text

--- client/src/components/FileViewer.jsx
+++ client/src/components/FileViewer.jsx
@@ -4,12 +4,14 @@ import axios from "axios"
 
 const FileViewer = () => {
 	const [files, setFiles] = useState([])
+	const [loading, setLoading] = useState(true)
 
 	const getAllFiles = async () => {
 		try {
 			const res = await axios.get("api/v1/files")
 			// console.log(res.data)
 			setFiles(res.data)
+			setLoading(false)
 		} catch (error) {
 			console.log(error)
 		}
@@ -20,34 +22,52 @@ const FileViewer = () => {
 	}, [])
 
 	return (
-		<div className="flex flex-wrap justify-center gap-4">
-			{files.map((f, i) => (
-				<div key={i} className="card w-96 bg-base-100 shadow-xl">
-					<figure className="px-10 pt-10">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							className="w-6 h-6">
-							<path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625z" />
-							<path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
-						</svg>
-					</figure>
-					<div className="card-body items-center text-center">
-						<h2 className="card-title">{f.fileName}</h2>
-						<p>{`File size: ${(Number(f.fileSize) / 1024).toFixed(
-							2
-						)} kB`}</p>
-						<div className="card-actions">
-							<Link
-								to={`/file/${f.fileData}`}
-								className="btn btn-accent">
-								View PDF
-							</Link>
-						</div>
-					</div>
+		<div>
+			{loading ? (
+				<div
+					className="radial-progress text-primary animate-spin"
+					style={{
+						"--value": 50,
+						"--size": "3rem",
+						"--thickness": "8px",
+					}}></div>
+			) : (
+				<div className="flex flex-wrap justify-center gap-4">
+					{files.length > 0 ? (
+						files.map((f, i) => (
+							<div
+								key={i}
+								className="card w-96 bg-base-100 shadow-xl">
+								<figure className="px-10 pt-10">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										fill="currentColor"
+										className="w-6 h-6">
+										<path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625z" />
+										<path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
+									</svg>
+								</figure>
+								<div className="card-body items-center text-center">
+									<h2 className="card-title">{f.fileName}</h2>
+									<p>{`File size: ${(
+										Number(f.fileSize) / 1024
+									).toFixed(2)} kB`}</p>
+									<div className="card-actions">
+										<Link
+											to={`/file/${f.fileData}`}
+											className="btn btn-accent">
+											View PDF
+										</Link>
+									</div>
+								</div>
+							</div>
+						))
+					) : (
+						<div>No Files Uploaded</div>
+					)}
 				</div>
-			))}
+			)}
 		</div>
 	)
 }
